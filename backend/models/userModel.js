1ae07059ba09a8147be6f72650b71bbb99edbda6
Jabo Jean Marie <jabojeanmarie5@gmail.com>
2023-06-28 16:42:13 +0200

    ...

--- backend/models/userModel.js
+++ backend/models/userModel.js
@@ -1,5 +1,4 @@
 import mongoose from "mongoose";
-import bcrypt from "bcryptjs";
 
 const userSchema = mongoose.Schema({
     name: {
@@ -19,18 +18,6 @@ const userSchema = mongoose.Schema({
     timestamps: true
 });
 
-// userSchema.pre('save', async function(next){
-//     if(!this.isModified('password')){
-//         next();
-//     }
-
-//     const salt = await bcrypt.genSalt(10);
-//     this.password = await bcrypt.hash(this.password, salt);
-// });
-
-// userSchema.methods.matchPasswords = async function(enteredPassword){
-//     return await bcrypt.compare(enteredPassword, this.password);
-// }
 
 const User = mongoose.model('User', userSchema);
 
